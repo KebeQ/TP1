9e245d858e07be5ca1e29f141a6397609aaa3510
KebeQ <45235498+KebeQ@users.noreply.github.com>
2020-04-23 12:09:16 -0400

    Update Artistes-DQL-Select.sql

--- Artistes-DQL-Select.sql
+++ Artistes-DQL-Select.sql
@@ -11,7 +11,7 @@ FROM
 WHERE 
         NOM_SCENE IS NOT NULL;        
 ORDER BY
-        PRENOM ASC;
+        NOM, PRENOM;
         
 /***********************************************************
         Script C2
@@ -40,7 +40,7 @@ SELECT
 FROM
         CLIENT, CONTRAT
 ORDER BY 
-        CLIENT.NOM ASC;
+        CLIENT.NOM;
 
 /***********************************************************
         Script C4
@@ -56,14 +56,55 @@ FROM
 WHERE
         MEMBRE.NO_ARTISTE IS NOT NULL AND MEMBRE.ROLE IS NOT NULL
 ORDER BY 
-        GROUPE.NOM ASC;
+        GROUPE.NOM;
+
+/***********************************************************
+        Script C5
+***********************************************************/
+SELECT
+        CLIENT.NOM,
+        CONTRAT.NO_CONTRAT,
+        CONTRAT.DATE_CONTRAT,
+        CONTRAT.DATE_PRESTATION,
+        CONTRAT.HEURE_DEBUT_PRESENTATION,
+        CONTRAT.HEURE_FIN_PRESTATION	
+FROM
+        CLIENT, CONTRAT
+WHERE
+        CONTRAT.DATE_CONTRAT BETWEEN '2019-09-01' AND '2019-10-31'
+ORDER  BY
+        CLIENT.NOM;
+
+/***********************************************************
+        Script C6
+***********************************************************/
+SELECT 
+        CONTRAT.NO_CONTRAT,
+        CONTRAT.DATE_CONTRAT,
+        CONTRAT.NOM_GROUPE,
+        GROUPE.CACHET_HABITUEL,
+        CONTRAT.CACHET_PREVUT	
+FROM
+        CONTRAT, GROUPE
+WHERE 
+        (GROUPE.CACHET_HABITUEL - CONTRAT.CACHET_PREVU) >= 500
+ORDER BY
+        GROUPE.CACHET_HABITUEL DESC;
         
-        
-       
-        
-        
-        
-        
-        
-  
-   
+/***********************************************************
+        Script C7
+***********************************************************/
+SELECT
+        FACTURE.NO_FACTURE,
+        FACTURE.DATE_FACTURE,
+        FACTURE.MONTANT_FACTURE,
+        CONTRAT.DATE_CONTRAT,
+        CONTRAT.CACHET_PREVUT,
+        CLIENT.NOM,
+        CLIENT.TELEPHONE
+FROM
+        FACTURE, CONTRAT, CLIENT
+WHERE
+        CLIENT.CODE IN ('418', '518')
+ORDER BY
+        FACTURE.MONTANT_FACTURE DESC;
