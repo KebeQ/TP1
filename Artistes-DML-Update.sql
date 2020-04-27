617b29af462616f7eadbd3d8c7ae583013010089
KebeQ <45235498+KebeQ@users.noreply.github.com>
2020-04-27 14:50:54 -0400

    Update Artistes-DML-Update.sql

--- Artistes-DML-Update.sql
+++ Artistes-DML-Update.sql
@@ -1,7 +1,19 @@
-UPDATE GROUP
-SET CACHET_HABITUEL = CACHET_HABITUEL + (0,035*CACHET_HABITUEL);
-
-UPDATE CONTRAT
-SET CACHET_PREVU = CACHET_PREVU - (0,10*CACHET_PREVU)
-WHERE HEURE_DEBUT_PRESTATION = 18 OR 19
-AND HEURE_FIN_PRESTATION = <22;
+/***********************************************************
+        Script D1 
+***********************************************************/	
+UPDATE
+        GROUPE
+SET
+        CACHET_HABITUEL = CACHET_HABITUEL + (CACHET_HABITUEL * 0.035);
+        
+/***********************************************************
+        Script D2
+***********************************************************/
+UPDATE
+        CONTRAT
+SET
+        CACHET_PREVUT = CACHET_PREVUT - (CACHET_PREVUT * 0.1)
+WHERE
+        (HEURE_DEBUT_PRESTATION BETWEEN 18 AND 19)
+        AND
+        HEURE_FIN_PRESTATION >= 22;
